55150d7a1c52ad8cb92f702c413716e1d2a77366
Donko <ddonkbienv@gmail.com>
2019-02-15 14:36:54 +0100

    Interface Employee

--- Employee.java
+++ Employee.java
@@ -1,6 +1,32 @@
 public class Employee{
-  String employeeId;
-  String firstName;
-  String lastName;
-  String jobTitle;
+  private int employeeId;
+  private String firstName;
+  private String lastName;
+  private String jobTitle;
+  
+  //les accesseur des attributs 
+  public void setEmployeeId(String employeeId) {
+      this.employeeId = employeeId;
+  }
+  public int getEmployeeId() {
+      return employeeId;
+  }
+  public void setFirstName(String firstName) {
+      this.firstName = firstName;
+  }
+  public String getFirstName() {
+      return firstName;
+  }
+  public void setLastName(String lastName) {
+      this.lastName = lastName;
+  }
+  public String getLastName() {
+      return lastName;
+  }
+  public void setJobTitle(String jobTitle) {
+      this.jobTitle = jobTitle;
+  }
+  public String getJobTitle() {
+      return jobTitle;
+  }
 }
---
+++ EmployeeDao.java
@@ -0,0 +1,20 @@
+import Employee;
+public interface EmployeeDao {
+    /**
+    * permet d'ajouter un nouvel employee
+    */
+    void ajouter( Employee employee ) throws DAOException;
+    /**
+    * supprimer un employee deja existant
+    */
+    Employee supprimer( int employeeId ) throws DAOException;
+    /**
+    * permet de recuperer la liste de tuous les employées
+    */
+    List<Employee> getListeEmployee() throws DAOException;
+    /**
+    * @param employeeId
+    * @return les détails d'un employer particulier
+    */
+    List<Employee> getEmployeeDetails(int employeeId) throws DAOException;
+}
