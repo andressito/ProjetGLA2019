2ed0148272d639b5cef936da50083a05885764c8
KENY andre <keny@informatique.univ-paris-diderot.fr>
2019-02-15 13:29:32 +0100

    couche persistance v1

---
+++ Employee.java
@@ -0,0 +1,7 @@
+
+public class Employee{
+  String employeeId;
+  String firstName;
+  String lastName;
+  String jobTitle;
+}
---
+++ Flight.java
@@ -0,0 +1,14 @@
+
+
+public class Flight{
+  String atcNumber;
+  String departureAirport;
+  String arrivalAiport;
+  String departureTime;
+  String arrivalTime;
+  String OFP;
+  ArrayList<String> notamList;
+  ArrayList<String> adList;
+  ArrayList<Employee> crewList;
+  String meteoCard;
+}
