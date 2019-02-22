b75e8d6afa861ee814067eacdc4b971edd76ad2b
KENY andre <keny@informatique.univ-paris-diderot.fr>
2019-02-22 08:35:42 +0100

    rangement

---
+++ classeTd/Ads.java
@@ -0,0 +1,5 @@
+
+public class Ads {
+	String numberAds;
+	String content;
+}
---
+++ classeTd/Flight.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Flight{
+  String atcNumber;
+  String departureAirport;
+  String arrivalAiport;
+  String departureTime; // à discuter
+  String arrivalTime;	// à discuter
+  String OFP;
+  List<String> notamList; 
+  List<Ads> adsList; 
+  List<Employee> crewList;
+  String meteoCard;
+}
