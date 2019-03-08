6dd12ec10770a32a15fe0ce1a3b537652a62ff06
andressito <andrekeny@gmail.com>
2019-03-08 23:57:51 +0100

    dao

---
+++ Projet/ProjetCode/jetty-jersey-master/src/main/java/com/example/jetty_jersey/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.example.jetty_jersey.dao;
+
+import com.example.jetty_jersey.classes.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    public List<User> getAllUser();
+}
