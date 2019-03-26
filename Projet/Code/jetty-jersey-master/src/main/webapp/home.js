9a89220470a42cdca78b6938085b3ea36b0c4579
andressito <andrekeny@gmail.com>
2019-03-26 08:58:22 +0100

    signOUt

--- Projet/Code/jetty-jersey-master/src/main/webapp/home.js
+++ Projet/Code/jetty-jersey-master/src/main/webapp/home.js
@@ -1,1 +1,30 @@
+$(document).ready(function() {
+    if(localStorage.getItem("save")){
+        if(sessionStorage.getItem("email")){
+            var email = sessionStorage.getItem("email");
+        }else{
+            var email=localStorage.getItem("save");
+            sessionStorage.setItem("email",email);
 
+        }
+        $("#menu2").show();
+        $("#menu1").hide();
+        $('#id1').val(email);
+    }else{
+        $("#menu1").show();
+        $("#menu2").hide();
+    }
+});
+
+$(function () {
+    $("#signOut").click(function () {
+        sessionStorage.clear();
+        localStorage.clear();
+        window.location.href="http://localhost:8080/";
+        swal({
+            title: "ChuChuFly!",
+            text: "Success Sign Out",
+            icon: "success"
+        });
+    });
+});
