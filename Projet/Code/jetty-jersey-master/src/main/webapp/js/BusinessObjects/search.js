ef244d340e6e6719612736d1b232edff4269c52c
andressito <andrekeny@gmail.com>
2019-04-10 13:29:59 +0200

    booking 1

--- Projet/Code/jetty-jersey-master/src/main/webapp/js/BusinessObjects/search.js
+++ Projet/Code/jetty-jersey-master/src/main/webapp/js/BusinessObjects/search.js
@@ -17,14 +17,60 @@ $(document).ready(function() {
 function detailsFlight(flightId) {
     console.log(flightId);
     $("#flightList").remove();
+    $.ajax({
+        url: "http://localhost:8080/ws/flight/flights/"+flightId,
+        type: "GET",
+        contentType: "application/json",
+        cache: false,
+        dataType: "json"
+    }).done(function (result) {
+        const flightId = result['flightId'];
+        const departureAerodrom = result['departureAerodrom'];
+        const type = result['type'];
+        const arrivalAerodrom = result['arrivalAerodrom'];
+        const remainingSeats= result['remainingSeats'];
+        const price = result['price'];
+        const userId = result['userId'];
+        const dateFlight = result['date'];
+        const departureTime= result['departureTime'];
+        const arrivalTime= result['arrivalTime'];
+        console.log(departureAerodrom);
+        var tr_str =
+            '<div class="col-md-3" ><div class="price-box">'+
+            '<h2 class="pricing-plan">' + type + '</h2>' +
+            '<div class="price"><sup class="currency">€</sup>' + price + '<small>/seat</small></div>' +
+            '<p> From :' + departureAerodrom + '</p>' +
+            '<p> To :' + arrivalAerodrom+ '</p>' +
+            '<p> Remaining Seats :' + remainingSeats+ '</p>' +
+            '<p> Pilot :' +  + '</p>' +
+            '<h6 id="idFlight1"> &#x2605; &#x2605; &#x2606; &#x2606; &#x2606; </h6>' +
+            '<a class="btn btn-select-plan btn-sm"   onclick="detailsFlight(\'' + flightId + '\');" >Details</a>' +
+            '</div></div>';
+        $("#flightList").append(tr_str);
+        var ress =
+            '<div class="col-md-5" ><div class="price-box">'+
+            '<h2 class="pricing-plan">' + type + '</h2>' +
+            '<div class="price"><sup class="currency">€</sup>' + price + '<small>/seat</small></div>' +
+            '<p> From :' + departureAerodrom + '</p>' +
+            '<p> Date :' + dateFlight + '</p>' +
+            '<p> Departure Time :' + departureTime + '</p>' +
+            '<p> Arrival Time :' + departureAerodrom + '</p>' +
+            '<p> To :' + arrivalTime+ '</p>' +
+            '<p> Remaining Seats :' + remainingSeats+ '</p>' +
+            '<p> Pilot :' +  + '</p>' +
+            '<h6 id="idFlight1"> &#x2605; &#x2605; &#x2606; &#x2606; &#x2606; </h6>' +
+            '<a class="btn btn-select-plan btn-sm"   onclick="detailsFlight(\'' + flightId + '\');" >Details</a>' +
+            '</div></div>';
+    });
+
 }
 
 function getFlightList2(){
     //recupération reussie
     var dpAero = sessionStorage.getItem("departureAerodrome");
     var dpDate = sessionStorage.getItem("departureDate");
-    sessionStorage.removeItem("departureAerodrome");
-    sessionStorage.removeItem("departureDate");
+    //sessionStorage.removeItem("departureAerodrome");
+    //sessionStorage.removeItem("departureDate");
     if(dpDate.length==0)
         dpDate="0";
     $.ajax({
@@ -63,8 +109,6 @@ function getFlightList2(){
                     '</div></div>';
                 $("#flightList").append(tr_str);
             });
-
         }
-
     });
 }
