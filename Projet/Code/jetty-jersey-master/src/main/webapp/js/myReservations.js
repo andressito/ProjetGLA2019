908ed7b08a624fa26b34f88ad1fbecc974b418d7
Hiba Alami <halami717@gmail.com>
2019-04-24 00:51:06 +0200

    myFlights for pilots and myReservations for all users (still working on it)

--- Projet/Code/jetty-jersey-master/src/main/webapp/js/myReservations.js
+++ Projet/Code/jetty-jersey-master/src/main/webapp/js/myReservations.js
@@ -1,78 +1,70 @@
 $(document).ready(function() {
-    if(localStorage.getItem("userId")){
+    if(localStorage.getItem("userId")) {
         var firstName;
         var typeUser;
         var userId;
-        if(sessionStorage.getItem("firstName") && sessionStorage.getItem("userId") && sessionStorage.getItem("typeUser")){
+        if (sessionStorage.getItem("firstName") && sessionStorage.getItem("userId") && sessionStorage.getItem("typeUser")) {
             firstName = sessionStorage.getItem("firstName");
-            typeUser =sessionStorage.getItem("typeUser");
+            typeUser = sessionStorage.getItem("typeUser");
             userId = sessionStorage.getItem("userId");
             sessionStorage.removeItem("departureAerodrome");
             sessionStorage.removeItem("departureDate");
-        }else{
+        } else {
             firstName = localStorage.getItem("firstName");
-            typeUser =localStorage.getItem("typeUser");
+            typeUser = localStorage.getItem("typeUser");
             userId = localStorage.getItem("userId");
-            sessionStorage.setItem("typeUser",typeUser);
-            sessionStorage.setItem("firstName",firstName);
-            sessionStorage.setItem("userId",userId);
+            sessionStorage.setItem("typeUser", typeUser);
+            sessionStorage.setItem("firstName", firstName);
+            sessionStorage.setItem("userId", userId);
         }
         document.getElementById("bg").style.backgroundImage = "url('images/paris.jpg')";
-        if(typeUser==="passenger"){
+        if (typeUser === "passenger") {
             $("#menu").load('../Menu/MenuPassenger.html');
-        }else{
+        } else {
             $("#menu").load('../Menu/MenuPilot.html');
             $.ajax({
-                url: "http://localhost:8080/ws/reservations/reservation/user/"+userId,
+                url: "http://localhost:8080/ws/reservations/reservation/user/" + userId,
                 type: "GET",
                 contentType: "application/json",
                 cache: false,
                 dataType: "json"
             }).done(function (result) {
-                console.log(result.length);
-                if(result.length>0){
-                    var table=
-                        "<div class=\"upcomingFlight\"><h1> "+userName+"'s reservations list </h1>"+
-                        "<br><br><br><table class=\"table\">"+
-                        "<thead>"+
+                if (result) {
+                    var table =
+                        "<div class=\"upcomingFlight\"><h1> Reservations list </h1>" +
+                        "<br><br><br><table class=\"table\">" +
+                        "<thead>" +
                         "<tr>" +
-                        "<td>Departure</td>"+
-                        "<td>Arrival</td>"+
-                        "<td>Date departure</td>"+
-                        "<td>Initial Seats</td>"+
-                        "<td>Remaining Seats</td>"+
-                        "<td>Details</td>"+
-                        "</tr>"+
-                        "</thead>"+
-                        "<tbody>"+
+                        "<td>Departure</td>" +
+                        "<td>Arrival</td>" +
+                        "<td>Date departure</td>" +
+                        "<td>Initial Seats</td>" +
+                        "<td>Remaining Seats</td>" +
+                        "<td>Details</td>" +
+                        "</tr>" +
+                        "</thead>" +
+                        "<tbody>" +
                         "<tr>" +
-                        "<td>"+result['departureAerodrom']+"</td>"+
-                        "<td>"+result['arrivalAerodrom']+"</td>"+
-                        "<td>"+result['date']+"</td>"+
-                        "<td>"+result['allSeats']+"</td>"+
-                        "<td>"+result['remainingSeats']+"</td>"+
-                        "<td> <button value='"+result['flightId']+"' onclick='DetailsFLightHome' class=\"btn-details\"> Details</button></td>"+
-                        "</tr>"+
-                        "</tbody>"+
-                        "</table>"+
+                        "<td>" + result['departureAerodrom'] + "</td>" +
+                        "<td>" + result['arrivalAerodrom'] + "</td>" +
+                        "<td>" + result['date'] + "</td>" +
+                        "<td>" + result['allSeats'] + "</td>" +
+                        "<td>" + result['remainingSeats'] + "</td>" +
+                        "<td> <button value='" + result['flightId'] + "' onclick='DetailsFLightHome' class=\"btn-details\"> Details</button></td>" +
+                        "</tr>" +
+                        "</tbody>" +
+                        "</table>" +
                         "</div>";
                     $("#myReservations").append(table);
-                }else{
-
-                    var table=
-                        "<div class=\"upcomingFlight\"><br><h1> No flights to display </h1>"+
-                        "<br>"+
-                        "<a href='AddFlight.html'><h5> Add a new flight here</h5></a>"+
+                } else {
+                    var table =
+                        "<div class=\"upcomingFlight\"><br><h1> No Reservations to display </h1>" +
+                        "<br>" +
+                        "<a href='home.html'><h5> Search for a convenient flight and book it here </h5></a>" +
                         "</div>";
-                    $("#flight").append(table);
+                    $("#myReservations").append(table);
                 }
-
             });
         }
-    }else{
-        $("#menu").load('../Menu/Menu.html');
-        document.getElementById("bg").style.backgroundImage = "url('images/slide_1.jpg')";
-        document.getElementById('search').style.display='inline';
-        document.getElementById('flight').style.display='none';
     }
 });
