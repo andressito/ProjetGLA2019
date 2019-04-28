156f98e3a484b5ff1e5c2bd3b01a0e2ce1bc520a
Hiba Alami <halami717@gmail.com>
2019-04-28 19:42:24 +0200

    about.html & about.js updated.

---
+++ Projet/Code/jetty-jersey-master/src/main/webapp/js/BusinessObjects/about.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    if(localStorage.getItem("userId")){
+        var firstName;
+        var typeUser;
+        var userId;
+        if(sessionStorage.getItem("firstName") && sessionStorage.getItem("userId") && sessionStorage.getItem("typeUser")){
+            firstName = sessionStorage.getItem("firstName");
+            typeUser =sessionStorage.getItem("typeUser");
+            userId = sessionStorage.getItem("userId");
+            sessionStorage.removeItem("departureAerodrome");
+            sessionStorage.removeItem("departureDate");
+        }else{
+            firstName = localStorage.getItem("firstName");
+            typeUser =localStorage.getItem("typeUser");
+            userId = localStorage.getItem("userId");
+            sessionStorage.setItem("typeUser",typeUser);
+            sessionStorage.setItem("firstName",firstName);
+            sessionStorage.setItem("userId",userId);
+        }
+        if(typeUser==="passenger"){
+            console.log("passenger");
+            $("#menu").load('../Menu/MenuPassenger.html');
+        }else{
+            $("#menu").load('../Menu/MenuPilot.html');
+        }
+    }else{
+        $("#menu").load('../Menu/Menu.html');
+    }
+});
