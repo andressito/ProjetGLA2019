a8aa34f4c22e648ce0e109abe41c83c940d4518a
andressito <andrekeny@gmail.com>
2019-04-30 11:49:48 +0200

    redirection homePilot vers home

--- Projet/Code/jetty-jersey-master/src/main/java/com/example/jetty_jersey/bouchonDAO/BouchonReservationDAO.java
+++ Projet/Code/jetty-jersey-master/src/main/java/com/example/jetty_jersey/bouchonDAO/BouchonReservationDAO.java
@@ -84,7 +84,7 @@ public class BouchonReservationDAO implements ReservationDAO {
 
 	public ArrayList<Reservation> getReservationByUserId(String userId) {
 		try {
-            return JettyMain.c.getReservationByUser(userId);
+            return JettyMain.c.getReservationByUserId(userId);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -93,7 +93,7 @@ public class BouchonReservationDAO implements ReservationDAO {
 
 	public ArrayList<Reservation> getReservationByFlight(String flight) {
 		try {
-            return JettyMain.c.getReservationByFlight(flight);
+            return JettyMain.c.getReservationByFlightId(flight);
         } catch (IOException e) {
             e.printStackTrace();
         }
